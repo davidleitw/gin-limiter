17fc839d7670e365d3035594d297611a6e86d752
davidlei <davidleitw@gmail.com>
2020-08-03 13:50:29 +0800

    lua script build(40/100) 思考流程

--- lua.go
+++ lua.go
@@ -3,27 +3,54 @@ package limiter
 const Script = `
 	local result = {}
 		
-	local now = tonumber(ARGV[1])
+	local now = tonumber(ARGV[1]) -- 現在時間
 	
-	local globalKey = KEYS[1]
+	local globalKey = KEYS[1] 
 	local singleKey = KEYS[2]
 	
 	local globalLimit = tonumber(ARGV[2])
 	local singleLimit = tonumber(ARGV[3])
+	local globalExpired = ARGV[4]
+	local singleExpired = ARGV[5]
 
 	local IpGlobalInfo = redis.call('HGETALL', globalKey)
 	local IpSingleInfo = redis.call('HGETALL', singleKey)
 	
+	if globalExpired == "true" then 
+		redis.call('HMSET', globalKey, "Count", 1)
+		result[1] = globalLimit - 1
+	end
+
+	if singleExpired == "true" then
+		redis.call('HMSET', singleKey, "Count", 1)
+		result[2] = singleLimit - 1
+	end
+
 	-- 該Ip第一次訪問
 	if #IpGlobalInfo == 0 then
-		return 0
+		redis.call('HMSET', globalKey, "Count", 1)
+		redis.call('HMSET', singleKey, "Count", 1)
+		result[1] = globalLimit - 1 
+		result[2] = singleLimit - 1 
+		return result
 	end
+
+	if #IpSingleInfo == 0 then 
+		redis.call('HMSET', singleKey, "Count", 1)
+		result[2] = singleLimit - 1
+	end
+
+	local gc = tonumber(IpGlobalInfo[2])
+	local sc = tonumber(IpSingleInfo[2])
+
+
 `
 
 const TestScript = `
 	local result = {}
 
 	-- 測試註解
+	result[5] = 28
 	result[1] = 10
 	result[2] = 20 + 2
 	result[3] = KEYS[1]
--- middleware.go
+++ middleware.go
@@ -12,6 +12,9 @@ func LimitMiddle(lc *LimitController) gin.HandlerFunc {
 	lc.Init()
 
 	return func(ctx *gin.Context) {
+		var globalExpired string = "false"
+		var singleExpired string = "false"
+
 		now := time.Now().Unix()
 		path := ctx.FullPath()
 		method := ctx.Request.Method
@@ -19,12 +22,23 @@ func LimitMiddle(lc *LimitController) gin.HandlerFunc {
 
 		globalKey := "Source:" + ipAddress // Source:123.456.78.9
 		globalLimit := lc.GetGlobalLimit()
+		globalDeadLine := lc.globalRate.GetDeadLine()
 
 		singleKey := path + "/" + method + "/" + ipAddress // /a/post/post/123.456.78.9
 		singleLimit := lc.GetSingleLimit(path, method)
+		singleDeadLine := lc.routerRates.GetDeadLine(path, method)
+
+		if now > globalDeadLine {
+			globalExpired = "true"
+			lc.globalRate.UpdateDeadLine()
+		}
+		if now > singleDeadLine {
+			singleExpired = "true"
+			lc.routerRates.UpdateDeadLine(path, method)
+		}
 
 		script := redis.NewScript(Script)
-		args := []interface{}{now, globalLimit, singleLimit}
+		args := []interface{}{now, globalLimit, singleLimit, globalExpired, singleExpired}
 		keys := []string{globalKey, singleKey}
 
 		result, err := script.Run(ctx, lc.RedisDB, keys, args).Result()
