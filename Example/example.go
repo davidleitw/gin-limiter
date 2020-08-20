9df30cace8c46599f7cd033ba817ee66aaf87419
davidlei <davidleitw@gmail.com>
2020-08-21 00:28:24 +0800

    update example.go, a simple example with gin

--- Example/example.go
+++ Example/example.go
@@ -1,35 +1,34 @@
 package main
 
+import (
+	"log"
+
+	"net/http"
+
+	limiter "github.com/davidleitw/gin-limiter"
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
+)
+
 func main() {
-	// server := gin.Default()
-	// rdb := redis.NewClient(&redis.Options{Addr: "localhost:6379", Password: "", DB: 0})
+	server := gin.Default()
+	rdb := redis.NewClient(&redis.Options{Addr: "localhost:6379", Password: "", DB: 0})
 
-	// limitControl, err := limiter.DefaultController(rdb, "24-M", 100, "debug") // Debug mode, each 24 minutes can send 100 times request from single Ip.
-	// if err != nil {
-	// 	log.Println(err)
-	// }
+	dispatcher, err := limiter.LimitDispatcher("24-M", 100, rdb)
+	if err != nil {
+		log.Println(err)
+	}
 
-	// err = limitControl.Add("/ExamplePost1", "POST", "4-M", 20) // "/ExamplePost1" route, each 4 minutes can send 20 times request from single Ip.
-	// if err != nil {
-	// 	log.Println(err)
-	// }
+	server.POST("/ExamplePost1", dispatcher.MiddleWare("4-M", 20), func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "Hello ExamplePost1")
+	})
 
-	// err = limitControl.Add("/ExampleGet1", "GET", "20-H", 40) // ".ExampleGet1" route, each 20 hours can send 40 times request from single Ip.
-	// if err != nil {
-	// 	log.Println(err)
-	// }
-	// server.Use(limitControl.GenerateLimitMiddleWare()) // create middleware
+	server.GET("/ExampleGet1", dispatcher.MiddleWare("5-M", 10), func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "Hello ExampleGet1")
+	})
 
-	// server.POST("/ExamplePost1", func(ctx *gin.Context) {
-	// 	ctx.String(200, "Hello Example! In ExamplePost1")
-	// })
-
-	// server.GET("/ExampleGet1", func(ctx *gin.Context) {
-	// 	ctx.String(200, "Hello Example! In ExampleGet1")
-	// })
-
-	// err = server.Run(":8080")
-	// if err != nil {
-	// 	log.Println("gin server run error = ", err)
-	// }
+	err = server.Run(":8080")
+	if err != nil {
+		log.Println("gin server error = ", err)
+	}
 }
